899c058a70e4ffa3c160d986633d45611209dd69
Summer Stevenson <50718811+SummerDarcel@users.noreply.github.com>
2019-09-12 13:06:17 -0700

    Update friends.js

--- FriendFinder/app/data/friends.js
+++ FriendFinder/app/data/friends.js
@@ -1,4 +1,25 @@
-var friends = []
+var friends = [
+    {
+        name: "Dolph",
+        photoURL: "https://www.imdb.com/name/nm0000185/mediaviewer/rm1793314816",
+        scores: ["3", "2", "1", "5", "4", "4", "3", "1", "2", "2"]
+    },
+    {
+        name: "Alexander",
+        photoURL: "https://fashionmagazine.com/culture/mens-fashion-the-talented-mr-skarsgard/",
+        scores: ["4", "4", "4", "3", "5", "2", "3", "5", "3", "5"]
+    },
+    {
+        name: "Oprah",
+        photoURL: "http://static.oprah.com/images/o2/201407/201407-omag-WhatIKnowForSure-949x534.jpg",
+        scores: ["5", "2", "2", "1", "3", "1", "3", "2", "5", "5"]
+    },
+    {
+        name: "Ellen",
+        photoURL: "https://i.ytimg.com/vi/EcACQJmjZek/maxresdefault.jpg",
+        scores: ["3", "4", "4", "3", "4", "3", "3", "5", "3", "2"]
+    }
+];
 
 
 module.exports = friendsArray;
